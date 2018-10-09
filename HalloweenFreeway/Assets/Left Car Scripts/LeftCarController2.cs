289941d718b1a50fddaaa9af52c4925de99d5824
LAPTOP-LHT7QQQG\zuko5 <tev218@nyu.edu>
2018-10-09 06:42:45 -0400

    Halloween Freeway

---
+++ HalloweenFreeway/Assets/Left Car Scripts/LeftCarController2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftCarController2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+		transform.Translate(Vector2.left * -0.2f, Space.World);
+		
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		if (collisionInfo.gameObject.tag == "RightTrigger")
+		{
+			Debug.Log("yuh");
+			transform.position = new Vector2(-18.75f, -3.51f);
+			
+			
+			//For an OnTriggerEnter2D for score count
+			//count1 = count1 + 1;
+			//SetCountText1();
+
+		}
+	}
+	
+}
